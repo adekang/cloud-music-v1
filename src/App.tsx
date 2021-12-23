3db0fa521104b48e29f543a393959086d8b2a990
adekang <adekang@163.com>
2021-12-23 22:03:52 +0800

    feat: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from "react"
 import { GlobalStyle } from "./style"
 import { IconStyle } from "./assets/iconfont/iconfont"
+import routes from "./routes/index.js"
+import { HashRouter } from "react-router-dom"
+import { renderRoutes } from "react-router-config"
 
 function App() {
 	return (
-		<div className='App'>
+		<HashRouter>
 			<GlobalStyle></GlobalStyle>
 			<IconStyle></IconStyle>
-			123
-			<a href='#12300'>链接</a>
-			<i className='iconfont'>&#xe62b;</i>
-		</div>
+			{renderRoutes(routes)}
+		</HashRouter>
 	)
 }
 
