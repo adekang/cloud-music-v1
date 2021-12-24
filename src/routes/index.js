4015412475d982405696bd241215a98c1c90fac7
adekang <adekang@163.com>
2021-12-24 15:36:48 +0800

    fix :routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,37 +1,36 @@
-import React from "react"
-import { Redirect } from "react-router-dom"
-import { RouteConfig } from "react-router-config"
-import Home from "../application/Home"
-import Recommend from "../application/Recommend"
-import Singers from "../application/Singers"
-import Rank from "../application/Singers"
+import React from 'react'
+import {Redirect} from 'react-router-dom'
+import Home from '../application/Home'
+import Recommend from '../application/Recommend'
+import Singers from '../application/Singers'
+import Rank from '../application/Singers'
 
 const routes = [
-	{
-		path: "/",
-		component: Home,
-		routes: [
-			{
-				path: "/",
-				exact: true,
-				render: () => {
-					return <Redirect to={'/admin/control'}/>
-				},
-			},
-			{
-				path: "/recommend",
-				component: Recommend,
-			},
-			{
-				path: "/singers",
-				component: Singers,
-			},
-			{
-				path: "/rank",
-				component: Rank,
-			},
-		],
-	},
+  {
+    path: '/',
+    component: Home,
+    routes: [
+      {
+        path: '/',
+        exact: true,
+        render: () => {
+          return <Redirect to={'/admin/control'}/>
+        },
+      },
+      {
+        path: '/recommend',
+        component: Recommend,
+      },
+      {
+        path: '/singers',
+        component: Singers,
+      },
+      {
+        path: '/rank',
+        component: Rank,
+      },
+    ],
+  },
 ]
 
 export default routes
