a1d8a05305812620a32be791871f10c54c222749
adekang <adekang@163.com>
2022-03-02 15:21:21 +0800

    feat: 一键部署

--- web.sh
+++ web.sh
@@ -3,8 +3,12 @@ yarn build &&
 cd build &&
 git init &&
 git add . &&
-git commit -m "update" &&
-git branch -M master &&
-git remote add origin git@github.com:adekang/cloud-music-web.git&&
-git push -f -u origin master &&
+git commit -m "deploy" &&
+# git branch -M master &&
+# git remote add origin git@github.com:adekang/cloud-music-web.git&&
+# git push -f -u origin master &&
+
+git branch -M gh-pages &&
+git push -f git@github.com:adekang/cloud-music-v1.git gh-pages &&
+
 cd -
